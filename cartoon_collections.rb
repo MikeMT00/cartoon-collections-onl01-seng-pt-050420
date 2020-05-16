cd53698c7d57d6b2e06376d816d22f704e410b5b
Michael Thomas <michaelthomas0611@gmail.com>
2020-05-16 17:52:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(array)              # code an argument here
 end
 
 def summon_captain_planet# code an argument here
-  # Your code here
+  array.collect { |word| word.capitalize + "!" }
 end
 
 def long_planeteer_calls# code an argument here
